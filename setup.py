bd84850c07be35fa369ec3077adb000307a1758f
Jeremy Brown <jeremynealbrown@gmail.com>
2013-12-24 14:49:54 -0800

    moved all files into pythonatron dir, added setup.py and __init__ files for each sub-module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    author = 'Jeremy Brown',
+    author_email = 'jeremynealbrown@gmail.com',
+    description = 'A home for my study of algorithms, networking, patterns and other musings',
+    name = 'Pythonatron',
+    license = 'gnu',
+    packages = find_packages(),
+    platforms = 'any',
+    url = 'https://github.com/jeremynealbrown/pythonatron',
+    version = '0.1',
+)
