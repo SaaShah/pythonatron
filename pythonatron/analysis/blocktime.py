3f39c62f03939f370f8f894acd9047a1147c76af
Jeremy Brown <jeremynealbrown@gmail.com>
2013-12-24 16:27:58 -0800

    added test for linear time function

--- pythonatron/analysis/blocktime.py
+++ pythonatron/analysis/blocktime.py
@@ -12,16 +12,35 @@ def run(entry, *args):
     return end - start
 
 if __name__ == '__main__':
-    def test(n):
+
+    def linear(n):
+        total = 0
+        for x in range(0, n):
+            total += x
+
+    def quadratic(n):
         total = 0
         for x in range(0, n):
             for y in range(0, n):
-                total += 2
+                total += x 
+    n1 = 100000
+    n2 = 1000000
+    
+    t1 = run(linear, n1)
+    t2 = run(linear, n2)
+    
+    print 'Linear'
+    print 'Time 1:', t1
+    print 'Time 2:', t2
+    print 'Order of growth:', empirical.order_of_growth(t1, t2, n1, n2)
+    
     n1 = 1000
     n2 = 10000
-    t1 = run(test, (n1))
-    t2 = run(test, (n2))
 
+    t1 = run(quadratic, n1)
+    t2 = run(quadratic, n2)
+
+    print '\nQuadratic'
     print 'Time 1:', t1
     print 'Time 2:', t2
     print 'Order of growth: ', empirical.order_of_growth(t1, t2, n1, n2)
